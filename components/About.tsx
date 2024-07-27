37d2670c3afaebca7be8dec431cb001110172d5d
Mikeale Alemu <mickeyalemu108@gmail.com>
2024-07-27 20:57:19 +0300

    about page finish

--- components/About.tsx
+++ components/About.tsx
@@ -20,14 +20,14 @@ const About = () => {
           className='object-cover object-center bg-cover opacity-80'
         />
       </div>
-      <main className='relative flex flex-wrap py-10 pb-32 gap-10 xl:gap-2 overflow-hidden lg:py-20 z-10'>
+      <main className='relative flex flex-col xl:flex-row py-10 pb-32 gap-10 xl:gap-2 overflow-hidden lg:py-20 z-10'>
         <motion.div
         ref={ref} 
         initial={{ x: -100, opacity: 0 }}
         animate={ isInView ? { x: 0, opacity: 1 } : {}}
         transition={{ delay:0.2, x: { type: 'spring', stiffness:60 } }}
         
-        className="relative z-20 order-1 xl:order-2 flex flex-1 items-end w-[611px]">
+        className="relative z-20 order-2 xl:order-1 items-end w-[611px] left-0 sm:left-10 md:left-20 lg:left-44 xl:left-0">
           <Image
             src='/Frame-1.png'
             alt='about image'
@@ -42,27 +42,29 @@ const About = () => {
         animate={ isInView ? { x: 0, opacity: 1 } : {}}
         transition={{ delay:0.2, x: { type: 'spring', stiffness:60 } }}
 
-        className="relative flex flex-1 order-2 xl:order-1 gap-6 items-start -translate-x-0 -translate-y-3 left-[4%] lg:left-[110px]
+        className="relative flex order-1 xl:order-2 gap-6 items-start -translate-x-0 -translate-y-3 left-[4%] lg:left-[110px]
           md:left-[6%] w-[598px]">
-          <h1 className='regular-40'>About Us</h1>
-          <div className="flex flex-col w-[598px] gap-4">
-            <p className='regular-18 w-[328px] xs:w-[387px] sm:w-[597px]'>
-              ENAT Foreign Employment Agent PLC is a leading overseas recruitment agency based
-              in Addis Ababa, Ethiopia, renowned for its exceptional services in connecting
-              qualified candidates with suitable job opportunities abroad. With a highly skilled team of professionals, we have established ourselves as a trusted name in the
-              industry.
-            </p>
-            <p className='regular-18 w-[328px] xs:w-[387px] sm:w-[597px]'>
-              One of our core strengths lies in our commitment to excellence. We prioritize
-              delivering high-quality services to both our clients and candidates.  
-            </p>
-            <p className='regular-18 w-[328px] xs:w-[387px] sm:w-[597px]'>
-              As a recognized agency, we specialize in providing Ethiopian housemaids to Saudi
-              Arabia, Dubai, and Jordan, meeting the demand for domestic help in these regions.
-              Furthermore, we have established agreements with Jordanian companies to supply
-              skilled and semi-skilled professionals, thus contributing to the development of the
-              workforce in Jordan.
-            </p>
+          <div className="flex flex-col">
+            <h1 className='regular-40'>About Us</h1>
+            <div className="flex flex-col w-[598px] gap-4">
+              <p className='regular-18 w-[328px] xs:w-[387px] sm:w-[597px]'>
+                ENAT Foreign Employment Agent PLC is a leading overseas recruitment agency based
+                in Addis Ababa, Ethiopia, renowned for its exceptional services in connecting
+                qualified candidates with suitable job opportunities abroad. With a highly skilled team of professionals, we have established ourselves as a trusted name in the
+                industry.
+              </p>
+              <p className='regular-18 w-[328px] xs:w-[387px] sm:w-[597px]'>
+                One of our core strengths lies in our commitment to excellence. We prioritize
+                delivering high-quality services to both our clients and candidates.  
+              </p>
+              <p className='regular-18 w-[328px] xs:w-[387px] sm:w-[597px]'>
+                As a recognized agency, we specialize in providing Ethiopian housemaids to Saudi
+                Arabia, Dubai, and Jordan, meeting the demand for domestic help in these regions.
+                Furthermore, we have established agreements with Jordanian companies to supply
+                skilled and semi-skilled professionals, thus contributing to the development of the
+                workforce in Jordan.
+              </p>
+            </div>
           </div>
         </motion.div>
       </main>
