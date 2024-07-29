9dd1c6af58964ad4eaf0455cf353e3d1f81f45f9
Mikeale Alemu <mickeyalemu108@gmail.com>
2024-07-29 19:00:46 +0300

    hero image fix it

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -41,13 +41,21 @@ const Hero = ({ autoSlide= false }) => {
             className="w-screen min-h-screen mix-blend-overlay bg-cover bg-center"
           />from-black/70 to-black/40
         </div> */}
-        <div className="w-full h-screen bg-gradient-to-t from-black/70 to-black/40">
-          <img
+        <div className="w-full bg-gradient-to-t from-black/70 to-black/40">
+          {/* <img
             src={`${sliderHome[currentIndex].url}`}
             alt='home image'
             // width={363}
             // height={684}
-            className="w-full bg-cover h-screen bg-no-repeat mix-blend-overlay bg-center top-0 left-0"
+            className="w-full bg-cover max-h-screen bg-no-repeat mix-blend-overlay bg-center top-0 left-0"
+          /> */}
+          <Image
+            src={`${sliderHome[currentIndex].url}`}
+            alt='Hero Image'
+            layout='responsive'
+            width={363}
+            height={284}
+            className="w-screen max-h-screen mix-blend-overlay bg-cover bg-center"
           />
         </div>
         {/* <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-1 md:right-6 sm:right-3 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
@@ -56,20 +64,20 @@ const Hero = ({ autoSlide= false }) => {
         <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-1 md:left-6 sm:left-3 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
           <BsChevronCompactLeft onClick={privSlide} size={30} />
         </div> */}
-        <div className="absolute bottom-16 right-0 left-0">
+        <div className="absolute bottom-3 xs:bottom-7 right-0 left-0">
           <div className="flex flex-row items-center justify-center gap-3">
             {sliderHome.map((_, i) => (
               <div key={i} className={`{transition-all w-2 h-2 bg-white rounded-full ${ currentIndex === i ? 'p-1' : "bg-opacity-50" }`} />
             ))}
           </div>
         </div>
-        <div className="absolute w-[759px] h-[294px] gap-2 -translate-x-0 translate-y-[50%] left-[6%] lg:left-[110px] md:left-[6%]">
-          <h1 className='text-white bold-18 md:bold-20 lg:bold-30'>Discover Your Next Adventure</h1>
-          <h1 className="w-[354px] sm:w-[435px] md:w-[487px] lg:w-[642px] xl:w-[737px] text-white bold-40 sm:bold-52 md:bold-60 lg:bold-75">
+        <div className="absolute w-[759px] h-[294px] gap-2 -translate-x-0 translate-y-[5%] xs:translate-y-[15%] sm:translate-y-[35%] md:translate-y-[45%] left-[6%] lg:left-[110px] md:left-[6%]">
+          <h1 className='text-white bold-14 xs:bold-18 md:bold-20 lg:bold-30'>Discover Your Next Adventure</h1>
+          <h1 className="w-[354px] sm:w-[435px] md:w-[487px] lg:w-[642px] xl:w-[737px] text-white bold-36 xs:bold-40 sm:bold-52 md:bold-60 lg:bold-75">
             Find your <span className="text-blue-300 font-black font-serif">dream</span> job abroad
           </h1>
           <div className="max-w-5xl lg:mr-auto pr-5 md:pr-10 xl:pr-0 w-full">
-            <p className="regular-14 md:regular-18 mt-6 text-white/80 w-[326px] xs:w-[386px] sm:w-[579px] md:w-[687px] lg:w-[769px] xl:w-[916px]">
+            <p className="regular-10 sm:regular-14 mt-1 sm:mt-6 text-white/80 w-[326px] xs:w-[386px] sm:w-[579px] md:w-[687px] lg:w-[769px] xl:w-[916px]">
               ENAT Foreign Employment Agent PLC is synonymous with reliability, professionalism, and efficiency. We prioritize the welfare of our candidates, 
               ensuring their safety and well-being throughout the entire recruitment process. With our extensive network, expertise, and unwavering commitment,
               we strive to deliver optimal results and create a positive impact on both our clients and candidates.
