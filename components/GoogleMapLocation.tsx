39fc5fd7456e621a77a98fae96f8912a78caf146
Mikeale Alemu <mickeyalemu108@gmail.com>
2024-07-30 10:32:58 +0300

    enat google map

--- components/GoogleMapLocation.tsx
+++ components/GoogleMapLocation.tsx
@@ -4,14 +4,15 @@ const GoogleMapLocation = () => {
 
   return (
     <iframe
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1140.661955043367!2d38.786358376786396!3d9.01425786180975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85e141c3cd9d%3A0xfc82dd6e5e7cd4da!2zWWVhYiBCdWlsZGluZyB8IEhheWFodWxldCB8IOGLqOGKoOGJpSDhiIXhipXhjLsgfCDhiIPhi6vhiIHhiIjhibU!5e0!3m2!1sen!2set!4v1720776101987!5m2!1sen!2set"
-    width="100%"
-    height="400"
-    style={{ border: 0 }}
-    allowFullScreen
-    loading="lazy"
-    referrerPolicy="no-referrer-when-downgrade"
-  ></iframe>
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15762.106136683258!2d38.787704!3d9.0156384!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85a7e4fbf351%3A0x4e646b1d1b8b1bb2!2sENAT%20FOREIGN%20EMPLOYMENT%20AGENT%20PLC!5e0!3m2!1sen!2set!4v1722324590943!5m2!1sen!2set"
+        width="100%"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="ENAT Foreign Employment Agent PLC Location"
+      ></iframe>
   )
 }
 
